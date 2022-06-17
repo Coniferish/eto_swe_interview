712de828c4c5acc6d7f57da5620ba91dd0d24d42
Coniferish <johnjennings702@gmail.com>
2022-06-17 10:35:31 -0400

    initial commit

---
+++ challenge.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+"""Minimal normalization of json file containing organization affiliation data from https://arxiv.org/"""
+
+import json
+import re
+import sys
+import csv
+
+if len(sys.argv) != 3:
+    raise IndexError(
+        "Please try running again with arguments <input_file_name> <output_filename>"
+    )
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+data = json.load(open(input_file, "r"))
+ALL_ORGS = [x["author_affiliation"] for x in data]
+ABBRS = {
+    "u": "University",
+    "uni": "University",
+    "univ": "University",
+    "dept": "Department",
+    "inst": "Institute",
+    "nat": "National",
+    "chem": "Chemistry",
+    "div": "Division",
+    "obs": "Observatory",
+    "phys": "Physics",
+    # 'tech': 'Technology' or 'Technological'
+}
+
+# this function needs to be separated into smaller functions to separately test
+# that the abbreviations list is being correctly generated
+# and that the test_orgs are changed correctly
+def fix_abbreviations(orgs: list, abbreviations: dict):
+    normalized = {}  # not included as a parameter because of testing
+    change_count = 0
+    for org in orgs:
+        if normalized.get(org):
+            normal = normalized[org]
+        else:
+            normal = org
+        for abbr in abbreviations:
+            normal = re.sub(rf"(?i)\b{abbr}($|\.|\b)", abbreviations[abbr], normal)
+            normalized[org] = normal
+        if normal != org:
+            change_count += 1
+    return normalized, change_count
+
+
+# TODO: normalize missing values: '', 'na', 'n/a', etc.
+# TODO: normalize orgs listed as one character
+# TODO: remove repeated phrases like "for the"
+
+if __name__ == "__main__":
+    normalized, change_count = fix_abbreviations(ALL_ORGS, ABBRS)
+    with open(output_file, "w") as f:
+        w = csv.writer(f, dialect="excel")
+        w.writerow(["original_affiliation", "normalized_affiliation"])
+        w.writerows(normalized.items())
+    print(f"File saved as {output_file}\n{change_count} corrections made")
---
+++ test_challenge.py
@@ -0,0 +1,60 @@
+import unittest
+import challenge
+
+# def capitalize(string):
+#     return string.capitalize()
+
+# def add_period(string):
+#     return string + '.'
+
+# def same(string):
+#     return string
+
+# def capital_period(string):
+#     return capitalize(add_period(string))
+
+# def all_abbrs(abbreviations: dict, variations: list):
+#     return { y(x):abbreviations[x] for x in abbreviations.keys() for y in variations }
+
+# VARIATIONS = [
+# capitalize,
+# add_period,
+# same,
+# capital_period
+# ]
+
+# # creating the list of abbreviations since the fix_abbreviations function uses regex to match against multiple cases
+# # due to time, I did not use this to test my regex pattern as intended
+# # TODO: write test for regex pattern
+# ALL_ABBRS = all_abbrs(challenge.abbreviations, VARIATIONS)
+
+ORGS = [
+    {"Technological University of Uruguay": "Technological University of Uruguay"},
+    {"uni. of somewhere": "University of somewhere"},
+    {"Uni of somewhere": "University of somewhere"},
+    {"Vienna, Tech. U.": "Vienna, Tech. University"},
+    {"Vienna, Tech. U": "Vienna, Tech. University"},
+    {"Uni of somewhere": "University of somewhere"},
+    {"Villanova University": "Villanova University"},
+    {"UC Davis": "UC Davis"},
+    {"U of GA, dept of math": "University of GA, Department of math"},
+]
+
+
+class FixAbbreviationsTest(unittest.TestCase):
+
+    # test that the list of ORGS returns their expected values
+    def test_all(self):
+        for i in ORGS:
+            with self.subTest(i=i):  # using subTest to test each individaul org in ORGS
+                # simply using "list(i.keys())" is returning "Error in argument: '(i.keys())'" for some reason
+                self.assertEqual(
+                    challenge.fix_abbreviations(
+                        [x for x in i.keys()], challenge.abbreviations
+                    ),
+                    i,
+                )
+
+
+if __name__ == "__main__":
+    unittest.main()

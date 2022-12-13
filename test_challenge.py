4ceba5445e224ea56acddd712782c2b8e71d6cef
Coniferish <johnjennings702@gmail.com>
2022-12-13 15:46:08 -0500

    update test

--- test_challenge.py
+++ test_challenge.py
@@ -3,30 +3,27 @@ import challenge
 
 
 TEST_ORGS = [
-    {"Technological University of Uruguay": "Technological University of Uruguay"},  # no change
-    {"uni. of somewhere": "University of somewhere"},  # abbreviation, case, and punctuation
-    {"Uni of somewhere": "University of somewhere"},  # abbreivation
-    {"Vienna, Tech. U.": "Vienna, Tech. University"},  # abbreviation and period, ending
-    {"Vienna, Tech. U": "Vienna, Tech. University"},  # abbreviation, ending
-    {"Uni of somewhere": "University of somewhere"},  # abbreviation
-    {"U of GA, dept of math": "University of GA, Department of math"},  # multiple abbreviation
+    ("Technological University of Uruguay", "Technological University of Uruguay"),  # no change
+    ("uni. of somewhere", "University of somewhere"),  # abbreviation, case, and punctuation
+    ("Uni of somewhere", "University of somewhere"),  # abbreivation
+    ("Vienna, Tech. U.", "Vienna, Tech. University"),  # abbreviation and period, ending
+    ("Vienna, Tech. U", "Vienna, Tech. University"),  # abbreviation, ending
+    ("Uni of somewhere", "University of somewhere"),  # abbreviation
+    ("U of GA, dept of math", "University of GA, Department of math"),  # multiple abbreviation
     # multiple abbreviation, case, and punctuation
-    {"nat. dept. of science at some uni.": "National Department of science at some University"},  
+    ("nat. dept. of science at some uni.", "National Department of science at some University") 
 ]
 
 
 class FixAbbreviationsTest(unittest.TestCase):
 
     # test that the list of TEST_ORGS returns their expected values
-    def test_all(self):
+    def test_normalize_name(self):
         for i in TEST_ORGS:
             with self.subTest(i=i):  # using subTest to test each individaul org in ORGS
                 # using "list(i.keys())" is returning "Error in argument: '(i.keys())'" for some reason
                 self.assertEqual(
-                    challenge.fix_abbreviations(
-                        [{"author_affiliation": x} for x in i.keys()], challenge.ABBRS
-                    ),
-                    i,
+                    challenge.normalize_name(i[0]), i[1]
                 )
 
 
